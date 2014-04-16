80b1b9e3f73779239c2bfa69197cb03dfa1904bd
Patti Jurgensen & Bailey Drake <student@epicodus.com>
2014-04-16 12:28:20 -0700

    setup mailcatcher and update README

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,4 @@
 class UsersController < ApplicationController
-  before_action :set_question, only: [:show, :edit, :update, :destroy]
 
-  def home
-  end
 
-  def show
-
-  end
-
-private
-  def set_question
-    @user = User.find(params[:id])
-  end
 end
