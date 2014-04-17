36c93eef66b2595f803dccdb99455a7ee76b24e8
Patti Jurgensen & Bailey Drake <student@epicodus.com>
2014-04-17 11:27:49 -0700

    add ability to send confirmation and locked out emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   has_many :rentals
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :registerable, :confirmable, :lockable
 
   validates :username, presence: true
 end
